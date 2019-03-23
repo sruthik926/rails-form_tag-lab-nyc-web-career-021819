651ea10c398724f786eae800d9e98f60990c7d6a
github username <github email address>
2019-03-23 10:45:18 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
-  
+
+
+  def new
+
+    render :new
+  end
+
   def index
     @students = Student.all
+    render :index
   end
 
   def show
     @student = Student.find(params[:id])
+    render :show
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
   end
 
 end
